f22f3fe866129a10cff440e29afe22ba14e39f1a
Utopia <utopiaio@yahoo.com>
2017-07-03 14:53:59 +0300

    Connected to routing
    - `LiwamController` moved to $run
    - not found template
    - initial call is now inside `$routeChangeSuccess`

--- app.js
+++ app.js
@@ -6,77 +6,81 @@
     angular.module('Liwam',['TodoService','ngRoute']);
     angular.module('Liwam')
         .config(function($routeProvider){
-            $routeProvider
-                .when('/:value',{
-                    templateUrl: "done.html",
-                    controller: "liwamController",
-
-                })
+          $routeProvider
+            .when('/:filter?', {
+                templateUrl: 'view/filter.html',
+            })
+            .otherwise({
+              templateUrl: 'view/not-found.html',
+            })
         })
-        .controller('liwamController',liwamController);
-    liwamController.$inject = ['$scope','Todo','$timeout','$routeParams','$rootScope'];
-    function liwamController ($scope,Todo,$timeout,$routeParams,$rootScope){
-             $scope.taskList = [];
-             $scope.taskListLength = 0;
-             $scope.task = '';
-             $scope.show = true;
-             $scope.routeVaraible = {vari:$routeParams.value}
-             $scope.Filter = 'All';
-             $scope.compeleted = 0;
+        .run(Run);
 
-             $rootScope.$on('$routeChangeSuccess',function(){
-                 $scope.show = false;
-             })
+        Run.$inject = ['$rootScope', '$routeParams', '$timeout', 'Todo'];
 
-            const compute = function(filter) {
-                Todo.getTask().then(function(tasks) {
-                  $timeout(function(){
-                    $scope.compeleted = tasks.filter(function (task) {
+        function Run($rootScope, $routeParams, $timeout, Todo) {
+          $rootScope.task = '';
+          $rootScope.taskList = [];
+          $rootScope.taskListLength = 0;
+          $rootScope.compeleted = 0;
+          $rootScope.filter = 'All';
+
+          $rootScope.add = function(){
+            $rootScope.taskList = Todo.addTask($rootScope.task);
+            $rootScope.task = '';
+          };
+
+          const compute = function(filter) {
+              Todo.getTask().then(function(tasks) {
+                $timeout(function(){
+                  $rootScope.compeleted = tasks.filter(function (task) {
+                    return task.done;
+                  }).length;
+
+                  $rootScope.taskListLength = tasks.length;
+                  $rootScope.taskList = tasks.filter(function (task) {
+                    switch (filter) {
+                      case 'All':
+                        return true;
+
+                      case 'Done':
                         return task.done;
-                    }).length;
-                    $scope.taskListLength = tasks.length;
-                    $scope.taskList = tasks.filter(function (task) {
-                        switch (filter) {
-                            case 'All':
-                                return true;
 
-                            case 'Done':
-                                return task.done;
+                      case 'Undone':
+                        return !task.done;
 
-                            case 'Undone':
-                                return !task.done;
-
-                            default:
-                                return true;
-                        }
-                    });
+                      default:
+                        return true;
+                    }
                   });
                 });
-            };
+              });
+          };
 
-           compute($scope.Filter);
+          $rootScope.$on('$routeChangeSuccess',function(event, toRoute) {
+            if (Object.prototype.hasOwnProperty.call(toRoute.params, 'filter')) {
+              compute(toRoute.params.filter);
+            } else {
+              compute('All');
+            }
+          });
 
-            $scope.add = function(){
-                $scope.taskList = Todo.addTask($scope.task);
-                $scope.task = '';
-            };
+          $rootScope.remove = function (taskId) {
+            Todo.removeTask(taskId);
+          };
 
-            $scope.remove = function (taskId) {
-                Todo.removeTask(taskId);
-            };
+          $rootScope.toggle = function (taskId) {
+            Todo.updateWhenToogled(taskId);
+          };
 
-            $scope.toggle = function (taskId){
-                Todo.updateWhenToogled(taskId);
-            };
+          $rootScope.setFilter = function(filter){
+            $rootScope.filter = filter;
+            compute(filter);
+          };
 
-            $scope.setFilter = function(filter){
-                $scope.Filter = filter;
-                compute(filter);
-            };
-
-            Todo.subscribe($scope,function() {
-              compute($scope.Filter);
-            });
-        };
+          Todo.subscribe($rootScope, function() {
+            compute($rootScope.filter);
+          });
+        }
 })(window.angular);
 
