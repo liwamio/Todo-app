2c88a7e5613d780b89edf8fd20e7900a671f5daf
liwam <ambayeliwam@gmail.com>
2017-07-10 15:14:22 -0700

    changed path to 'all'

--- app/controller/login.controller.js
+++ app/controller/login.controller.js
@@ -1,14 +1,17 @@
 /**
  * Created by user on 7/10/2017.*/
 (function (angular) {
-    angular.module('Liwam').controller('loginController',
+    angular.module('Liwam').controller('pathController',
         ['$location', 'Authenticate',
-            function logControllerInit($location, Authenticate) {
+            function pathControllerInit($location, Authenticate) {
                 Authenticate
                     .isLoggedIn()
                     .then(function (user) {
                         if (user === true) {
-                            $location.path('/home').replace();
+                            $location.path('/All').replace();
+                        }
+                        else{
+                            $location.path(Authenticate.getPath()).replace();
                         }
                     });
             }]);
